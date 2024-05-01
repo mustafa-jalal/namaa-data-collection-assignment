dcf002c7ffd93b4fd0dc891e5c2fc6629e98e89f
mustafa <mustafa.jalal2@gmail.com>
2024-05-01 15:15:18 +0300

    create v1/users api route

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\API\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('/v1/users', [UserController::class, 'index']);
